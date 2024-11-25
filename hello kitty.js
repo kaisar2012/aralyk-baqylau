3ed2d113c565dac8c389aad15446e13dec5eef7e
Kaisar <kaisar.bolatkan@gmail.com>
2024-11-25 19:39:13 +0500

    kot

---
+++ hello kitty.js
@@ -0,0 +1,69 @@
+// let san1 = +prompt("San engiz")
+// let san2 = +prompt("Ekinshi san engiz")
+
+
+// if (san1 + san2) 
+//     alert(san2 + san1)
+// else  if (san1 - san2){
+//     (san2 - san1)   
+// } else if(san1 / san2){
+//     (san2 / san1)
+// } else if(san1 * san2){
+//     (san2 * san1)
+// }
+// alert("Kate san engizdiniz")
+
+
+
+// Work 2
+
+
+// let san1 = +prompt("Zat engiziniz")
+// let baga = 4999;
+
+// if(baga < 5000){
+//     alert("Biz bankrot bolamyz! Sizge 10% skidka")
+// }else if(baga < 10000){
+//     alert("Olemiz boldy zat alma!Sizge 20% Skidka")
+// }else if(baga < 20000){
+//     alert("Biz sagan eshtenke satpaymyz!Sizge 30% skidka")
+// } else if(baga > 5000,10000,20000){
+//     alert("Rakhmet!Sizge skidka zhok")
+// }
+// alert("Rakhmet")
+
+
+// Work 3
+
+
+// function tabu(){
+
+//     var oi_san = parseInt(Math.random() * 100)//15
+//     console.log(oi_san)
+
+//     while(true){
+//         var san = +prompt("Sandy tap")//30
+
+//         if(oi_san == san){
+//             return "Durys"
+//         }
+//         if(san - oi_san <= 5  && oi_san - san <= 5){
+//             alert("Жақын")
+//         }else{
+//             alert("Алыс")
+//         }
+
+//     }
+
+// }
+// alert(tabu())
+
+
+
+
+
+
+
+
+
+
